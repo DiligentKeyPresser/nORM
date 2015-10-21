23217cb5476fc6f6be02b3598bdcd4aad6cb34b2
DiligentKeyPresser <tkachev.daniil@gmail.com>
2015-10-21 15:44:47 +1000

    more files have been separated

---
+++ nORM/DatabaseContext.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Runtime.CompilerServices;
+
+namespace nORM
+{
+    /// <summary> Обработчик события создания текста SQL команды. Предназначается для мониторинга работы оболочки. </summary>
+    /// <param name="CommandText"> Текст созданной команды </param>
+    public delegate void BasicCommandHandler(string CommandText);
+
+    internal abstract class DatabaseContext : IDatabase
+    {
+        public string Host { get; }
+        public string Database { get; }
+
+        private readonly string ConnectionString;
+
+        public event BasicCommandHandler BeforeCommandExecute;
+
+        internal DatabaseContext(string host, string database, string user, string password)
+        {
+            Host = host;
+            Database = database;
+            ConnectionString = string.Format("Data Source={0};Initial Catalog={1};Persist Security Info=True;User ID={2};Password={3};", host, database, user, password);
+        }
+
+        internal object ExecuteScalar(string Query)
+        {
+            using (var connection = new SqlConnection(ConnectionString))
+            using (var command = new SqlCommand(Query, connection))
+            {
+                if (BeforeCommandExecute != null) BeforeCommandExecute(Query);
+
+                connection.Open();
+                return command.ExecuteScalar();
+            }
+        }
+
+        internal IEnumerable<TElement> ExecuteProjection<TElement>(string Query, Func<object[], TElement> Projection)
+        {
+            using (var connection = new SqlConnection(ConnectionString))
+            using (var command = new SqlCommand(Query, connection))
+            {
+                if (BeforeCommandExecute != null) BeforeCommandExecute(Query);
+
+                connection.Open();
+                using (var reader = command.ExecuteReader())
+                {
+                    int ColumnCount = reader.FieldCount;
+                    var row = new object[ColumnCount];
+
+                    // список нужен, иначе будет возвращаться итератор по уничтоженному IDisposable
+                    var result = new List<TElement>();
+#warning нельзя ли заранее узнать размер?
+                    while (reader.Read())
+                    {
+                        reader.GetValues(row);
+                        result.Add(Projection(row));
+                    }
+                    return result;
+                }
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        internal IEnumerable<RowContract> ExecuteContract<RowContract>(string Query) => ExecuteProjection(Query, RowContractInflater<RowContract>.Inflate);
+    }
+}
--- nORM/nORM.cs
+++ nORM/nORM.cs
@@ -1,78 +1,14 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Data.SqlClient;
 using System.Linq;
 using System.Linq.Expressions;
-using System.Runtime.CompilerServices;
 
 #warning сунуть как можно больше проверок в дебаг
 #warning материализация в список - вероятно проблема. фикс должен коснуться как материализации, так и метода выполнения в контексте бд
 
 namespace nORM
 {
-    /// <summary> Обработчик события создания текста SQL команды. Предназначается для мониторинга работы оболочки. </summary>
-    /// <param name="CommandText"> Текст созданной команды </param>
-    public delegate void BasicCommandHandler(string CommandText);
-         
-    public abstract class DatabaseContext: IDatabase
-    {
-        public string Host { get; }
-        public string Database { get; }
-
-        private readonly string ConnectionString;
-
-        public event BasicCommandHandler BeforeCommandExecute;
-
-        internal DatabaseContext(string host, string database, string user, string password)
-        {
-            Host = host;
-            Database = database;
-            ConnectionString = string.Format("Data Source={0};Initial Catalog={1};Persist Security Info=True;User ID={2};Password={3};", host, database, user, password);
-        }
-
-        internal object ExecuteScalar(string Query)
-        {
-            using (var connection = new SqlConnection(ConnectionString))
-            using (var command = new SqlCommand(Query, connection))
-            {
-                if (BeforeCommandExecute != null) BeforeCommandExecute(Query);
-
-                connection.Open();
-                return command.ExecuteScalar();
-            }
-        }
-
-        internal IEnumerable<TElement> ExecuteProjection<TElement>(string Query, Func<object[], TElement> Projection)
-        {
-            using (var connection = new SqlConnection(ConnectionString))
-            using (var command = new SqlCommand(Query, connection))
-            {
-                if (BeforeCommandExecute != null) BeforeCommandExecute(Query);
-
-                connection.Open();
-                using (var reader = command.ExecuteReader())
-                {
-                    int ColumnCount = reader.FieldCount;
-                    var row = new object[ColumnCount];
-
-                    // список нужен, иначе будет возвращаться итератор по уничтоженному IDisposable
-                    var result = new List<TElement>();
-#warning нельзя ли заранее узнать размер?
-                    while (reader.Read())
-                    {
-                        reader.GetValues(row);
-                        result.Add(Projection(row));
-                    }
-                    return result;
-                }
-            }
-        }
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        internal IEnumerable<RowContract> ExecuteContract<RowContract>(string Query) => ExecuteProjection(Query, RowContractInflater<RowContract>.Inflate);
-    }
-        
 #warning непонятно, нужен ли такой тип
     internal abstract class DatabaseRow { }
     
@@ -116,7 +52,7 @@ namespace nORM
         public Type ElementType { get { return contract_type; } }
         public Expression Expression { get; }
         public IQueryProvider Provider { get { return RowProvider<RowContract>.Singleton; } }
-        public RowSource(DatabaseContext ConnectionContext) : base(ConnectionContext)
+        internal RowSource(DatabaseContext ConnectionContext) : base(ConnectionContext)
         {
             Expression = Expression.Constant(this);
         }
@@ -237,182 +173,4 @@ namespace nORM
 
     }
     */
-
-    internal static class PredicateTranslator
-    {
-        public static string[] TranslatePredicate<RowContract>(Expression E)
-        {
-#warning добавить предварительное вычисление
-#warning обрабатываются только предикаты с одним аргументом
-            return ToSQL<RowContract>(E, null);
-        }
-
-        private static string[] MakeBinary(string[] Left, string op, string[] Right)
-        {
-            var result = new string[Left.Length + 1 + Right.Length];
-            Array.Copy(Left, result, Left.Length);
-            result[Left.Length] = op;
-            Array.Copy(Right, 0, result, Left.Length + 1, Right.Length);
-            return result;
-        }
-
-        private static string[] ToSQL<RowContract>(Expression E, Expression Row)
-        {
-            var e_constant = E as ConstantExpression;
-            if (e_constant != null)
-            {
-                if (e_constant.Type == TypeOf.String) return new string[] { (string)e_constant.Value };
-                if (e_constant.Type == TypeOf.Int32) goto to_string;
-                if (e_constant.Type == TypeOf.Int16) goto to_string;
-
-#warning add debug output
-                return null;
-            to_string:
-                return new string[] { e_constant.Value.ToString() };
-            }
-
-            var e_member = E as MemberExpression;
-            if (e_member != null)
-            {
-                var RowPropertyAccess = e_member.Expression == Row;
-                if (RowPropertyAccess)
-                {
-#if DEBUG
-                    if (!Attribute.IsDefined(e_member.Member, TypeOf.FieldAttribute)) throw new InvalidContractException(typeof(RowContract), "Field name is not defined.");
-#endif
-#warning не самый быстрый способ. не закешировать ли?
-                    return new string[] { (Attribute.GetCustomAttribute(e_member.Member, TypeOf.FieldAttribute) as FieldAttribute).ColumnName };
-                }
-
-
-#warning add debug output
-                return null;
-            }
-
-            var e_binary = E as BinaryExpression;
-            if (e_binary != null)
-            {
-#warning do lifted calls need any special processing?
-#warning does 'method' need any special processing?
-
-                var Left = ToSQL<RowContract>(e_binary.Left, Row);
-                var Right = ToSQL<RowContract>(e_binary.Right, Row);
-
-                switch (e_binary.NodeType)
-                {
-                    case ExpressionType.Equal: return MakeBinary(Left, "=", Right);
-                    case ExpressionType.GreaterThan: return MakeBinary(Left, ">", Right);
-                    case ExpressionType.GreaterThanOrEqual: return MakeBinary(Left, ">=", Right);
-                    case ExpressionType.LessThan: return MakeBinary(Left, "<", Right);
-                    case ExpressionType.LessThanOrEqual: return MakeBinary(Left, "<=", Right);
-
-                    case ExpressionType.Add:
-                    case ExpressionType.AddChecked:
-                    case ExpressionType.And:
-                    case ExpressionType.AndAlso:
-                    case ExpressionType.ArrayLength:
-                    case ExpressionType.ArrayIndex:
-                    case ExpressionType.Call:
-                    case ExpressionType.Coalesce:
-                    case ExpressionType.Conditional:
-                    case ExpressionType.Convert:
-                    case ExpressionType.ConvertChecked:
-                    case ExpressionType.Divide:
-                    case ExpressionType.ExclusiveOr:
-                    case ExpressionType.Invoke:
-                    case ExpressionType.Lambda:
-                    case ExpressionType.LeftShift:
-                    case ExpressionType.ListInit:
-                    case ExpressionType.MemberAccess:
-                    case ExpressionType.MemberInit:
-                    case ExpressionType.Modulo:
-                    case ExpressionType.Multiply:
-                    case ExpressionType.MultiplyChecked:
-                    case ExpressionType.Negate:
-                    case ExpressionType.UnaryPlus:
-                    case ExpressionType.NegateChecked:
-                    case ExpressionType.New:
-                    case ExpressionType.NewArrayInit:
-                    case ExpressionType.NewArrayBounds:
-                    case ExpressionType.Not:
-                    case ExpressionType.NotEqual:
-                    case ExpressionType.Or:
-                    case ExpressionType.OrElse:
-                    case ExpressionType.Parameter:
-                    case ExpressionType.Power:
-                    case ExpressionType.Quote:
-                    case ExpressionType.RightShift:
-                    case ExpressionType.Subtract:
-                    case ExpressionType.SubtractChecked:
-                    case ExpressionType.TypeAs:
-                    case ExpressionType.TypeIs:
-                    case ExpressionType.Assign:
-                    case ExpressionType.Block:
-                    case ExpressionType.DebugInfo:
-                    case ExpressionType.Decrement:
-                    case ExpressionType.Dynamic:
-                    case ExpressionType.Default:
-                    case ExpressionType.Extension:
-                    case ExpressionType.Goto:
-                    case ExpressionType.Increment:
-                    case ExpressionType.Index:
-                    case ExpressionType.Label:
-                    case ExpressionType.RuntimeVariables:
-                    case ExpressionType.Loop:
-                    case ExpressionType.Switch:
-                    case ExpressionType.Throw:
-                    case ExpressionType.Try:
-                    case ExpressionType.Unbox:
-                    case ExpressionType.AddAssign:
-                    case ExpressionType.AndAssign:
-                    case ExpressionType.DivideAssign:
-                    case ExpressionType.ExclusiveOrAssign:
-                    case ExpressionType.LeftShiftAssign:
-                    case ExpressionType.ModuloAssign:
-                    case ExpressionType.MultiplyAssign:
-                    case ExpressionType.OrAssign:
-                    case ExpressionType.PowerAssign:
-                    case ExpressionType.RightShiftAssign:
-                    case ExpressionType.SubtractAssign:
-                    case ExpressionType.AddAssignChecked:
-                    case ExpressionType.MultiplyAssignChecked:
-                    case ExpressionType.SubtractAssignChecked:
-                    case ExpressionType.PreIncrementAssign:
-                    case ExpressionType.PreDecrementAssign:
-                    case ExpressionType.PostIncrementAssign:
-                    case ExpressionType.PostDecrementAssign:
-                    case ExpressionType.TypeEqual:
-                    case ExpressionType.OnesComplement:
-                    case ExpressionType.IsTrue:
-                    case ExpressionType.IsFalse:
-#warning add debug output
-                        return null;
-
-                    case ExpressionType.Constant:
-                    default:
-                        throw new InvalidProgramException("some unknown binary operator has been passed: " + e_binary.NodeType.ToString());
-                }
-            }
-
-            var e_lambda = E as LambdaExpression;
-            if (e_lambda != null) return ToSQL<RowContract>(e_lambda.Body, e_lambda.Parameters[0]);
-
-            var e_unary = E as UnaryExpression;
-            if (e_unary != null)
-            {
-#if DEBUG
-                if (e_unary.Operand == null) throw new NotImplementedException("where clause: unary expressions without operand are not supported");
-#endif
-                return ToSQL<RowContract>(e_unary.Operand, null);
-            }
-
-
-
-#warning add debug output
-            return null;
-        }
-
-
-
-    }
 }
