6efd282f963d427c1591f58b9ff4d6bc5678f11a
DiligentKeyPresser <tkachev.daniil@gmail.com>
2015-10-23 19:03:52 +1000

    basic 'Materializer' implementation

---
+++ nORM/Materialization.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace nORM
+{
+    internal static class Materializer
+    {
+#warning lazyness should be checked
+        private sealed class LazyEnumerator<RowContract> : IEnumerable<RowContract>
+        {
+            private readonly RowSource<RowContract> Source;
+            public LazyEnumerator(RowSource<RowContract> Source)
+            {
+                this.Source = Source;
+            }
+
+            public IEnumerator<RowContract> GetEnumerator()
+            {
+#warning действительно ли нет смысла возвращать обернутый reader?
+                var materialized = Source.ToArray();
+                foreach (var m in materialized) yield return m;
+            }
+
+            IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+        }
+
+        /// <summary>
+        /// Perfroms lazy materialization of IQueryable
+        /// </summary>
+        public static IQueryable<RowContract> Materialize<RowContract>(RowSource<RowContract> Source) => new LazyEnumerator<RowContract>(Source).AsQueryable();
+    }
+}
