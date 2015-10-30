894cb1a6adcad76e90edd463f696f351a963ea41
DiligentKeyPresser <tkachev.daniil@gmail.com>
2015-10-30 19:13:18 +1000

    1

---
+++ ExpLess.Test/Evaluator.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq.Expressions;
+
+namespace ExpLess.Test
+{
+    [TestClass]
+    public class Evaluator
+    {
+        [TestMethod]
+        public void Test1()
+        {
+
+        }
+    }
+}
