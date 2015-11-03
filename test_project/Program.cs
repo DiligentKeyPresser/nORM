1816eccdbff5463daf61199393b89cab9e3f1ccd
DiligentKeyPresser <tkachev.daniil@gmail.com>
2015-11-03 17:05:38 +1000

    merge

--- test_project/Program.cs
+++ test_project/Program.cs
@@ -47,7 +47,7 @@ namespace test_project
             };
             // "(tri ^ 2)" => UB???
             // tri << nint??0   - isLifted
-            Console.WriteLine(CopyDatabase.Table1.Any(r => new List<int>() { Capacity = 180 }.GetHashCode() > 5)); 
+           // Console.WriteLine(CopyDatabase.Table1.Any(r => new List<int>() { Capacity = 180 }.GetHashCode() > 5)); 
 
 
             Debugger.Break();
