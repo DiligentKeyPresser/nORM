94a065cf67d776a41f5c210afbb5c120534dd252
DiligentKeyPresser <tkachev.daniil@gmail.com>
2015-11-05 23:16:35 +1000

    better query building

---
+++ nORM/SqlConstructor/TSQL.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+
+namespace nORM
+{
+    namespace SQL
+    {
+        internal sealed class TSQLSelectQuery : SelectQuery
+        {
+            protected string[] fields;
+            protected string source;
+
+            public TSQLSelectQuery(string source, string[] fields)
+            {
+                this.source = source;
+                this.fields = fields;
+            }
+
+            protected override string Build()
+            {
+                var builder = new StringBuilder();
+
+                builder.Append("SELECT ");
+
+                for (int i = 0; i < fields.Length; i++)
+                {
+                    builder.Append(fields[i]);
+                    builder.Append(i < fields.Length - 1 ? ", " : " ");
+                }
+
+                builder.Append("FROM ");
+
+                builder.Append(source);
+
+                return builder.ToString();
+            }
+        }
+    }
+}
