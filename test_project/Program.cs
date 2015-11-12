c15f9757b92744ee973f10671edfec7a8acb5914
DiligentKeyPresser <tkachev.daniil@gmail.com>
2015-11-12 21:28:19 +1000

    fody is installed

--- test_project/Program.cs
+++ test_project/Program.cs
@@ -35,7 +35,7 @@ namespace test_project
 
         static void Main(string[] args)
         {
-            var CopyDatabase = Database<ITestDB>.Inflate(new SqlServerConnector("federalcom", "normtest", "normuser", "normpass"));
+            var CopyDatabase = Database<ITestDB>.Inflate(new SqlServerConnector("server", "normtest", "normuser", "normpass"));
             CopyDatabase.BeforeCommandExecute += Console.WriteLine;
 
             Console.WriteLine("partial evaluation");
@@ -61,7 +61,7 @@ namespace test_project
             Console.WriteLine(CopyDatabase.Table1.All(r=>r.ID > 1));
 
             Console.WriteLine();
-            Debugger.Break();
+         //   Debugger.Break();
 
             Console.WriteLine("any");
 
@@ -123,7 +123,8 @@ namespace test_project
             Console.WriteLine("simple select");
             // foreach (var r in CopyDatabase.Брак.Select(r=>r.Text)) Console.WriteLine(r);*/
 
-            Debugger.Break();
+            //Debugger.Break();
+            Console.ReadKey();
         }
     }
 
