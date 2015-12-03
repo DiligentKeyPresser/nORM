fca8bf4b4a87d2f351323d6fb220335af01a3ead
DiligentKeyPresser <tkachev.daniil@gmail.com>
2015-12-03 19:00:11 +1000

    ExpLess -> F#

---
+++ ExpLess.F/PartialEvaluator.fs
@@ -0,0 +1,15 @@
+﻿module ExpLess.PartialEvaluator 
+
+open System
+open System.Linq.Expressions
+
+let rec public PreEvaluate (E : Expression) = 
+    match E.NodeType with
+    | ExpressionType.Quote -> 
+        let e_unary = E :?> UnaryExpression
+        match e_unary.Method with
+        | null -> match PreEvaluate e_unary.Operand with
+                  | new_operand when new_operand = e_unary.Operand -> E
+                  | new_operand -> Expression.MakeUnary(ExpressionType.Quote, new_operand, E.Type) :> Expression
+        | _    -> raise(new NotImplementedException "ExpLess::PreEvaluate - Quote 'Method' is not implemented yet.") 
+    | _ -> E
