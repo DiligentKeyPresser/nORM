68420ec7b66d70e7026d80b91a342a7298a617f9
DiligentKeyPresser <tkachev.daniil@gmail.com>
2015-12-11 23:40:59 +1000

    invert

--- ExpLess.F/ExpressionInvertor.fs
+++ ExpLess.F/ExpressionInvertor.fs
@@ -4,10 +4,12 @@ open System
 open System.Linq.Expressions
 open Breakdown
 
-let rec public Invert (E : Expression) : Expression = 
-    match categorize E with    
-    | Lambda L                     -> upcast Expression.Lambda(Invert L.Body, L.Parameters) 
-    | Quote op                     -> upcast Expression.MakeUnary(E.NodeType, Invert op, E.Type) 
+let rec internal invert (tree : ExpressionNode) : ExpressionNode = 
+    match tree with    
+    | Lambda (body, args)          -> Lambda(invert body, args)
+    | Quote op                     -> Quote (invert op)
     | Unary (Not, exp)             -> exp
-    | _ when E.Type = typeof<bool> -> upcast Expression.MakeUnary(ExpressionType.Not, E, E.Type)
-    | _                            -> raise (new NotImplementedException ("Expression '" + E.ToString() + "' cannot be inverted"))
+    | _                            ->
+            let expr = inflate tree
+            if expr.Type = typeof<bool> then Unary (Not, tree)
+            else raise (new NotImplementedException ("Expression '" + expr.ToString() + "' cannot be inverted"))
